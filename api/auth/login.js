cdf8bd669a5d3ebf5d19d45469c63014e4aeba4e
blgzm18-collab <blgzm18@gmail.com>
2026-07-30 04:44:49 -0400

    Add Discord OAuth2 login redirect handler

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,1 +1,4 @@
-
+export default function handler(req, res) {
+  const redirect = `https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.DISCORD_REDIRECT_URI)}&response_type=code&scope=identify%20email`;
+  res.redirect(redirect);
+}
